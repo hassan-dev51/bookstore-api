0cc019ca0ee266466de707c9ceeb6d559a90ca63
hassan-dev51 <hassanmunir363@gmail.com>
2024-01-15 23:46:21 +0500

    first commit

---
+++ book.api.js
@@ -0,0 +1,32 @@
+const hotArrival = [
+  {
+    id: 1,
+    price: 90,
+    image:
+      "https://res.cloudinary.com/digitalvotingappimageupload/image/upload/v1705343967/image_3_zittt1.png",
+  },
+  {
+    id: 2,
+    price: 30,
+    image:
+      "https://res.cloudinary.com/digitalvotingappimageupload/image/upload/v1705343966/image_5_bdpqlc.png",
+  },
+  {
+    id: 3,
+    price: 930,
+    image:
+      "https://res.cloudinary.com/digitalvotingappimageupload/image/upload/v1705343966/image_4_hh1ctr.png",
+  },
+  {
+    id: 4,
+    price: 190,
+    image:
+      "https://res.cloudinary.com/digitalvotingappimageupload/image/upload/v1705343966/image_6_ynopgm.png",
+  },
+  {
+    id: 5,
+    price: 80,
+    image:
+      "https://res.cloudinary.com/digitalvotingappimageupload/image/upload/v1705343404/image_2_i2njf1.png",
+  },
+];
